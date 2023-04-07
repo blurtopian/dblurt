9f52a767f344cc87b1d5ab6f4c957c381a4e7d37
Jonathan Salatan <jsalatan@mcgi.org>
2023-04-08 01:19:54 +0900

    bump v0.1.10

--- src/chain/misc.ts
+++ src/chain/misc.ts
@@ -96,6 +96,182 @@ export interface ChainProperties {
   hbd_interest_rate: number; // uint16_t
 }
 
+/** Config of Blurt RPC node */
+export interface RpcNodeConfig {
+  IS_TEST_NET:                   false
+  BLURT_REDUCED_VOTE_POWER_RATE: number
+  BLURT_100_PERCENT:             number
+  BLURT_1_PERCENT:               number
+  BLURT_ACCOUNT_RECOVERY_REQUEST_EXPIRATION_PERIOD: string
+  BLURT_ACTIVE_CHALLENGE_COOLDOWN:                  string
+  BLURT_ACTIVE_CHALLENGE_FEE: {
+      amount:    string
+      precision: number
+      nai:       string
+  }
+  BLURT_ADDRESS_PREFIX:                       string
+  BLURT_APR_PERCENT_MULTIPLY_PER_BLOCK:       string
+  BLURT_APR_PERCENT_MULTIPLY_PER_HOUR:        string
+  BLURT_APR_PERCENT_MULTIPLY_PER_ROUND:       string
+  BLURT_APR_PERCENT_SHIFT_PER_BLOCK:          number
+  BLURT_APR_PERCENT_SHIFT_PER_HOUR:           number
+  BLURT_APR_PERCENT_SHIFT_PER_ROUND:          number
+  BLURT_BANDWIDTH_AVERAGE_WINDOW_SECONDS:     number
+  BLURT_BANDWIDTH_PRECISION:                  number
+  BLURT_BENEFICIARY_LIMIT:                    number
+  BLURT_BLOCKCHAIN_PRECISION:                 number
+  BLURT_BLOCKCHAIN_PRECISION_DIGITS:          number
+  BLURT_BLOCKCHAIN_HARDFORK_VERSION:          string
+  BLURT_BLOCKCHAIN_VERSION:                   string
+  BLURT_BLOCK_INTERVAL:                       number
+  BLURT_BLOCKS_PER_DAY:                       number
+  BLURT_BLOCKS_PER_HOUR:                      number
+  BLURT_BLOCKS_PER_YEAR:                      number
+  BLURT_CASHOUT_WINDOW_SECONDS:               number
+  BLURT_CHAIN_ID:                             string
+  BLURT_COMMENT_TITLE_LIMIT:                  number
+  BLURT_REWARD_CONSTANT:                      string
+  BLURT_CONTENT_REWARD_PERCENT_HF21:          number
+  BLURT_CUSTOM_OP_DATA_MAX_LENGTH:            number
+  BLURT_CUSTOM_OP_ID_MAX_LENGTH:              number
+  BLURT_GENESIS_TIME:                         string
+  BLURT_HARDFORK_REQUIRED_WITNESSES:          number
+  BLURT_HF21_CONVERGENT_LINEAR_RECENT_CLAIMS: string
+  BLURT_INFLATION_NARROWING_PERIOD:           number
+  BLURT_INFLATION_RATE_START_PERCENT:         number
+  BLURT_INFLATION_RATE_STOP_PERCENT:          number
+  BLURT_INIT_MINER_NAME:                      string
+  BLURT_INIT_PUBLIC_KEY_STR:                  string
+  BLURT_INIT_SUPPLY:                          string
+  BLURT_IRREVERSIBLE_THRESHOLD:               number
+  BLURT_MAX_ACCOUNT_CREATION_FEE:             number
+  BLURT_MAX_ACCOUNT_NAME_LENGTH:              number
+  BLURT_MAX_ACCOUNT_WITNESS_VOTES:            number
+  BLURT_MAX_ASSET_WHITELIST_AUTHORITIES:      number
+  BLURT_MAX_AUTHORITY_MEMBERSHIP:             number
+  BLURT_MAX_BLOCK_SIZE:                       number
+  BLURT_SOFT_MAX_BLOCK_SIZE:                  number
+  BLURT_MAX_CASHOUT_WINDOW_SECONDS:           number
+  BLURT_MAX_COMMENT_DEPTH:                    number
+  BLURT_MAX_INSTANCE_ID:                      string
+  BLURT_MAX_MEMO_SIZE:                        number
+  BLURT_MAX_WITNESSES:                        number
+  BLURT_MAX_PERMLINK_LENGTH:                  number
+  BLURT_MAX_PROXY_RECURSION_DEPTH:            number
+  BLURT_MAX_RUNNER_WITNESSES_HF17:            number
+  BLURT_MAX_SATOSHIS:                         string
+  BLURT_MAX_SHARE_SUPPLY:                     string
+  BLURT_MAX_SIG_CHECK_DEPTH:                  number
+  BLURT_MAX_SIG_CHECK_ACCOUNTS:               number
+  BLURT_MAX_TIME_UNTIL_EXPIRATION:            number
+  BLURT_MAX_TRANSACTION_SIZE:                 number
+  BLURT_MAX_UNDO_HISTORY:                     number
+  BLURT_MAX_URL_LENGTH:                       number
+  BLURT_MAX_VOTE_CHANGES:                     number
+  BLURT_MAX_VOTED_WITNESSES_HF17:             number
+  BLURT_MAX_WITHDRAW_ROUTES:                  number
+  BLURT_MAX_WITNESS_URL_LENGTH:               number
+  BLURT_MIN_ACCOUNT_CREATION_FEE:             number
+  BLURT_MIN_ACCOUNT_NAME_LENGTH:              number
+  BLURT_MIN_BLOCK_SIZE_LIMIT:                 number
+  BLURT_MIN_BLOCK_SIZE:                       number
+  BLURT_MIN_CONTENT_REWARD: {
+      amount:    string
+      precision: number
+      nai:       string
+  }
+  BLURT_MIN_CURATE_REWARD: {
+      amount:    string
+      precision: number
+      nai:       string
+  }
+  BLURT_MIN_PERMLINK_LENGTH:       number
+  BLURT_MIN_REPLY_INTERVAL_HF20:   number
+  BLURT_MIN_ROOT_COMMENT_INTERVAL: number
+  BLURT_MIN_COMMENT_EDIT_INTERVAL: number
+  BLURT_MIN_VOTE_INTERVAL_SEC:     number
+  BLURT_MINER_ACCOUNT:             string
+  BLURT_MIN_PAYOUT: {
+      amount:    string
+      precision: number
+      nai:       string
+  }
+  BLURT_MIN_PRODUCER_REWARD: {
+      amount:    string
+      precision: number
+      nai:       string
+  }
+  BLURT_MIN_TRANSACTION_EXPIRATION_LIMIT: number
+  BLURT_MIN_TRANSACTION_SIZE_LIMIT:       number
+  BLURT_MIN_UNDO_HISTORY:                 number
+  BLURT_NULL_ACCOUNT:                     string
+  BLURT_OWNER_AUTH_RECOVERY_PERIOD:       string
+  BLURT_OWNER_CHALLENGE_COOLDOWN:         string
+  BLURT_OWNER_CHALLENGE_FEE: {
+      amount:    string
+      precision: number
+      nai:       string
+  }
+  BLURT_OWNER_UPDATE_LIMIT:                  number
+  BLURT_POST_REWARD_FUND_NAME:               string
+  BLURT_PROXY_TO_SELF_ACCOUNT:               string
+  BLURT_SECONDS_PER_YEAR:                    number
+  BLURT_PROPOSAL_FUND_PERCENT_HF21:          number
+  BLURT_RECENT_RSHARES_DECAY_TIME_HF19:      string
+  BLURT_REVERSE_AUCTION_WINDOW_SECONDS_HF21: number
+  BLURT_ROOT_POST_PARENT:                    string
+  BLURT_SAVINGS_WITHDRAW_REQUEST_LIMIT:      number
+  BLURT_SAVINGS_WITHDRAW_TIME:               string
+  BLURT_SOFT_MAX_COMMENT_DEPTH:              number
+  BLURT_TEMP_ACCOUNT:                        string
+  BLURT_UPVOTE_LOCKOUT_HF17:                 string
+  BLURT_UPVOTE_LOCKOUT_SECONDS:              number
+  BLURT_VESTING_FUND_PERCENT_HF16:           number
+  BLURT_VESTING_WITHDRAW_INTERVALS:          number
+  BLURT_VESTING_WITHDRAW_INTERVALS_HF5:      number
+  BLURT_VESTING_WITHDRAW_INTERVAL_SECONDS:   number
+  BLURT_VOTE_DUST_THRESHOLD:                 number
+  BLURT_VOTING_MANA_REGENERATION_SECONDS:    number
+  BLURT_SYMBOL: {
+      nai:      string
+      decimals: number
+  }
+  VESTS_SYMBOL: {
+      nai:      string
+      decimals: number
+  }
+  BLURT_VIRTUAL_SCHEDULE_LAP_LENGTH2:        string
+  BLURT_DELEGATION_RETURN_PERIOD_HF20:       number
+  BLURT_RD_MIN_DECAY_BITS:                   number
+  BLURT_RD_MAX_DECAY_BITS:                   number
+  BLURT_RD_DECAY_DENOM_SHIFT:                number
+  BLURT_RD_MAX_POOL_BITS:                    number
+  BLURT_RD_MAX_BUDGET_1:                     string
+  BLURT_RD_MAX_BUDGET_2:                     number
+  BLURT_RD_MAX_BUDGET_3:                     number
+  BLURT_RD_MAX_BUDGET:                       number
+  BLURT_RD_MIN_DECAY:                        number
+  BLURT_RD_MIN_BUDGET:                       number
+  BLURT_RD_MAX_DECAY:                        number
+  BLURT_ACCOUNT_SUBSIDY_PRECISION:           number
+  BLURT_WITNESS_SUBSIDY_BUDGET_PERCENT:      number
+  BLURT_WITNESS_SUBSIDY_DECAY_PERCENT:       number
+  BLURT_DEFAULT_ACCOUNT_SUBSIDY_DECAY:       number
+  BLURT_DEFAULT_ACCOUNT_SUBSIDY_BUDGET:      number
+  BLURT_DECAY_BACKSTOP_PERCENT:              number
+  BLURT_BLOCK_GENERATION_POSTPONED_TX_LIMIT: number
+  BLURT_PENDING_TRANSACTION_EXECUTION_LIMIT: number
+  BLURT_TREASURY_ACCOUNT:                    string
+  BLURT_TREASURY_FEE:                        number
+  BLURT_PROPOSAL_MAINTENANCE_PERIOD:         number
+  BLURT_PROPOSAL_MAINTENANCE_CLEANUP:        number
+  BLURT_PROPOSAL_SUBJECT_MAX_LENGTH:         number
+  BLURT_PROPOSAL_MAX_IDS_NUMBER:             number
+  BLURT_PROPOSAL_MAX_END_DATE:               number
+  BLURT_PROPOSAL_EXPIRATION_UNFUNDED:        number
+  BLURT_INIT_POST_REWARD_BALANCE:            number
+}
+
 export interface VestingDelegation {
   /**
    * Delegation id.
@@ -153,20 +329,15 @@ export interface DynamicGlobalProperties {
    * Total asset held in confidential balances.
    */
   confidential_supply: Asset | string;
-  current_hbd_supply: Asset | string;
-  /**
-   * Total asset held in confidential balances.
-   */
-  confidential_hbd_supply: Asset | string;
-  total_vesting_fund_hive: Asset | string;
+  total_vesting_fund_blurt: Asset | string;
   total_vesting_shares: Asset | string;
-  total_reward_fund_hive: Asset | string;
+  total_reward_fund_blurt: Asset | string;
   /**
    * The running total of REWARD^2.
    */
   total_reward_shares2: string;
   pending_rewarded_vesting_shares: Asset | string;
-  pending_rewarded_vesting_hive: Asset | string;
+  pending_rewarded_vesting_blurt: Asset | string;
   /**
    * This property defines the interest rate that HBD deposits receive.
    */
@@ -232,10 +403,10 @@ export interface DynamicGlobalProperties {
  */
 export function getVestingSharePrice(props: DynamicGlobalProperties): Price {
   // empty string is needed to skip the type check error
-  const totalVestingFund = Asset.from(props.total_vesting_fund_hive);
+  const totalVestingFund = Asset.from(props.total_vesting_fund_blurt);
   const totalVestingShares = Asset.from(props.total_vesting_shares);
   if (totalVestingFund.amount === 0 || totalVestingShares.amount === 0) {
-    return new Price(new Asset(1, "VESTS"), new Asset(1, "HIVE"));
+    return new Price(new Asset(1, "VESTS"), new Asset(1, "BLURT"));
   }
   return new Price(totalVestingShares, totalVestingFund);
 }
@@ -265,4 +436,18 @@ export function getVests(
   }
 
   return vests.amount;
+}
+
+/** Reward Fund (post) */
+export interface RewardFund {
+  id:                       number
+  name:                     'post'
+  reward_balance:           Asset
+  recent_claims:            string
+  last_update:              string // YYYY-MM-DDTHH:mm:ss
+  content_constant:         string
+  percent_curation_rewards: number
+  percent_content_rewards:  number
+  author_reward_curve:      'convergent_linear'
+  curation_reward_curve:    'convergent_square_root'
 }
